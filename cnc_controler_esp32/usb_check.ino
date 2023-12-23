f133c8a4a171eb72cf852190265d3d2a1d65f415
MohammadFarajollahi <mohammadfarajollahi70@gmail.com>
2023-12-23 14:47:13 +0330

    v_2/10/1402

---
+++ cnc_controler_esp32/usb_check.ino
@@ -0,0 +1,25 @@
+
+
+void usb_check() {
+
+  if (flash_test == 0) {
+    flash_test = 1;
+    if (flashDrive.getDeviceStatus()) {
+      Serial.println(F("Flash drive attached!"));
+      drawSdJpeg("/usb_on.jpg", 430, 30);
+    } else {
+      Serial.println(F("Flash drive detached!"));
+      drawSdJpeg("/usb_off.jpg", 430, 30);
+    }
+  }
+
+  if (flashDrive.checkIntMessage()) {
+    if (flashDrive.getDeviceStatus()) {
+      Serial.println(F("Flash drive attached!"));
+      drawSdJpeg("/usb_on.jpg", 430, 30);
+    } else {
+      Serial.println(F("Flash drive detached!"));
+      drawSdJpeg("/usb_off.jpg", 430, 30);
+    }
+  }
+}
